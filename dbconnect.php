ddf6e1a0af24ef5a7ed481d94a333070106458b1
TimBlokdijk <1026135@mborijnland.nl>
2023-10-31 10:56:54 +0100

    dbname verandert

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $servername =   "localhost";
 $username   =   "root";
 $password   =   "";
-$dbname     =   "php5_02";
+$dbname     =   "";
 
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
