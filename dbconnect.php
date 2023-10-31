7f453978bc16c07eabb7deb72d7f765c69ffe383
TimBlokdijk <1026135@mborijnland.nl>
2023-10-31 12:02:51 +0100

    dbconnest dbname
    De nieuwe dbname is hackathon

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 $servername =   "localhost";
 $username   =   "root";
 $password   =   "";
-$dbname     =   "";
+$dbname     =   "hackathon";
 
 try{
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
