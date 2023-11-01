e2ab87fe597474e226d96de1d5943bfe5c8295f5
sjouk jonker <sjoukjonker13@gmail.com>
2023-11-01 14:47:46 +0100

    asds

--- index.php
+++ index.php
@@ -5,42 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
-<body>
-    <h2>Login</h2>
-    <form method="post">
-        Gebruikersnaam: <input type="text" name="gebruikersnaam"><br>
-        Wachtwoord: <input type="password" name="wachtwoord"><br>
-        <input type="submit" value="Inloggen">
-    </form>
 
-    <?php
-    if (isset($_POST["gebruikersnaam"]) && isset($_POST["wachtwoord"])) {
-        require "dbconnect.php";
+<h1>welkom</h1>
+<a href="inlog.php"> Login</a><br>
+<?php
+require_once "conn.php";
 
-        // Gebruikersnaam en wachtwoord van het formulier ophalen
-        $gebruikersnaam = $_POST["gebruikersnaam"];
-        $wachtwoord = $_POST["wachtwoord"];
-
-        // Query voor het controleren van gebruikersnaam en wachtwoord
-        $query = "SELECT * FROM gebruikers WHERE gebruikersnaam = :gebruikersnaam AND wachtwoord = :wachtwoord";
-        $stmt = $conn->prepare($query);
-        $stmt->bindParam(':gebruikersnaam', $gebruikersnaam);
-        $stmt->bindParam(':wachtwoord', $wachtwoord);
-        $stmt->execute();
-
-        if ($stmt->rowCount() > 0) {
-            // Inloggen gelukt
-            session_start();
-            $_SESSION["gebruikersnaam"] = $gebruikersnaam;
-            echo "Inloggen geslaagd. Welkom, $gebruikersnaam!";
-            header("Location: dashboard.php");
-        } else {
-            // Inloggen mislukt
-            echo "Inloggen mislukt. Controleer je gegevens.";
-        }
+    session_start();
+if (isset($_SESSION['gebruikersnaam'])) {
+        $gebruikersnaam = $_SESSION['gebruikersnaam'];
+        echo "Welkom  $gebruikersnaam ! <br>";
+        echo "<a href='dashboard.php'>dashboard</a> <br>";
+      
+    } else {
+        echo "Welkom, gast!";
+        echo "als iet wil doen moet je inlooggen <br>";
     }
-    ?>
+
+?>
 </body>
 </html>
 
-//verander gebruikersnaam naar email
